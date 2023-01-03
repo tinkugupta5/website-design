61d85282a45a4f44330d0202b57e752a06c4b086
tinku gupta <tinku.gupta4@gmail.com>
2023-01-03 21:56:11 +0530

    making get request

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './Featured.css'
 import BTC from '../assets/btc-img.png'
-import {FiArrowUpRight,FiArrowDown} from 'react-icons/'
+import {FiArrowUpRight,FiArrowDown} from 'react-icons/fi'
+import axios from 'axios'
 
 
 const Featured = () => {
+  const [data,setData] = useState(null)
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false'
+
+  useEffect(() => {
+    // eslint-disable-next-line no-unused-expressions
+    axios.get(url)
+    .then((response) => {
+      setData(response.data);
+    }).catch((error) => {
+      console.log(error)
+    })
+    
+  },[])
+
+  console.log(data)
+
+  if(!data) return null
+
+  // 49:24
+  
+
   return (
     <div className='featured'>
       <div className='container'>
