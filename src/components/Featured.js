7dc1e7c82f7c54de15682c4c0ea87cad45fba51c
tinku gupta <tinku.gupta4@gmail.com>
2023-01-05 00:51:03 +0530

    adding features section done

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -39,20 +39,144 @@ const Featured = () => {
         </div>
 
         {/* right */}
-
-        <div className='right'>
+        <div className='card'>
             <div className="top">
-            <img src={BTC} alt=''/>
+            {/* <img src={BTC} alt=''/> */}
+            <img src={data[0].image} alt=""/>
             </div>
             <div>
-              <h5>Bitcoin</h5>
-              <p>$49,000</p>
+              <h5>{data[0].name}</h5>
+              <p>${data[0].current_price.toLocaleString()}</p>
             </div>
-            <span><FiArrowUpRight/>2.5%</span>
+            {/* <span><FiArrowUpRight/>2.5%</span> */}
+
+            {/* logiv for price up color or down color  */}
+
+            {data[0].price_change_percentage_24h < 0 ? (
+              <span className='green'><FiArrowUpRight/>
+              {data[0].price_change_percentage_24h.toFixed(2)}%</span>
+            ):
+            (<span className='red'><FiArrowUpRight/>
+            {data[0].price_change_percentage_24h.toFixed(2)}%</span>)           
+            }
+
+
         </div>
+        <div className='card'>
+            <div className="top">
+            {/* <img src={BTC} alt=''/> */}
+            <img src={data[1].image} alt=""/>
+            </div>
+            <div>
+              <h5>{data[1].name}</h5>
+              <p>${data[1].current_price.toLocaleString()}</p>
+            </div>
+            {/* <span><FiArrowUpRight/>2.5%</span> */}
+
+            {/* logiv for price up color or down color  */}
+
+            {data[1].price_change_percentage_24h < 0 ? (
+              <span className='green'><FiArrowUpRight/>
+              {data[1].price_change_percentage_24h.toFixed(2)}%</span>
+            ):
+            (<span className='red'><FiArrowUpRight/>
+            {data[1].price_change_percentage_24h.toFixed(2)}%</span>)           
+            }
 
 
+        </div>
+        <div className='card'>
+            <div className="top">
+            {/* <img src={BTC} alt=''/> */}
+            <img src={data[2].image} alt=""/>
+            </div>
+            <div>
+              <h5>{data[2].name}</h5>
+              <p>${data[2].current_price.toLocaleString()}</p>
+            </div>
+            {/* <span><FiArrowUpRight/>2.5%</span> */}
 
+            {/* logiv for price up color or down color  */}
+
+            {data[2].price_change_percentage_24h < 0 ? (
+              <span className='green'><FiArrowUpRight/>
+              {data[2].price_change_percentage_24h.toFixed(2)}%</span>
+            ):
+            (<span className='red'><FiArrowUpRight/>
+            {data[2].price_change_percentage_24h.toFixed(2)}%</span>)           
+            }
+
+
+        </div>
+        <div className='card'>
+            <div className="top">
+            {/* <img src={BTC} alt=''/> */}
+            <img src={data[3].image} alt=""/>
+            </div>
+            <div>
+              <h5>{data[3].name}</h5>
+              <p>${data[3].current_price.toLocaleString()}</p>
+            </div>
+            {/* <span><FiArrowUpRight/>2.5%</span> */}
+
+            {/* logiv for price up color or down color  */}
+
+            {data[3].price_change_percentage_24h < 0 ? (
+              <span className='green'><FiArrowUpRight/>
+              {data[3].price_change_percentage_24h.toFixed(2)}%</span>
+            ):
+            (<span className='red'><FiArrowUpRight/>
+            {data[3].price_change_percentage_24h.toFixed(2)}%</span>)           
+            }
+
+
+        </div>
+        <div className='card'>
+            <div className="top">
+            {/* <img src={BTC} alt=''/> */}
+            <img src={data[4].image} alt=""/>
+            </div>
+            <div>
+              <h5>{data[4].name}</h5>
+              <p>${data[4].current_price.toLocaleString()}</p>
+            </div>
+            {/* <span><FiArrowUpRight/>2.5%</span> */}
+
+            {/* logiv for price up color or down color  */}
+
+            {data[4].price_change_percentage_24h < 0 ? (
+              <span className='green'><FiArrowUpRight/>
+              {data[4].price_change_percentage_24h.toFixed(2)}%</span>
+            ):
+            (<span className='red'><FiArrowUpRight/>
+            {data[4].price_change_percentage_24h.toFixed(2)}%</span>)           
+            }
+
+
+        </div>
+        <div className='card'>
+            <div className="top">
+            {/* <img src={BTC} alt=''/> */}
+            <img src={data[5].image} alt=""/>
+            </div>
+            <div>
+              <h5>{data[5].name}</h5>
+              <p>${data[5].current_price.toLocaleString()}</p>
+            </div>
+            {/* <span><FiArrowUpRight/>2.5%</span> */}
+
+            {/* logiv for price up color or down color  */}
+
+            {data[5].price_change_percentage_24h < 0 ? (
+              <span className='green'><FiArrowUpRight/>
+              {data[5].price_change_percentage_24h.toFixed(2)}%</span>
+            ):
+            (<span className='red'><FiArrowUpRight/>
+            {data[5].price_change_percentage_24h.toFixed(2)}%</span>)           
+            }
+
+
+        </div>
       </div>
     </div>
   )
